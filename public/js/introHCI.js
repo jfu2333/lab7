6cfe3707f8484f1bc42df32d39e307d77b20340e
Jiayu Fu <j3fu@ucsd.edu>
2020-02-20 13:22:03 -0800

    .

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,10 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(likeBtnClicked);
+}
+
+function likeBtnClicked(event){
+
+	ga("send","event","like","click");
 }
